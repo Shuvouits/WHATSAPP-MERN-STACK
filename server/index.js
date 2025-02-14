f96a460d95b3a6a3c10051c0e3e02b81b55d441c
Shuvo Bhowmik <41848224+Shuvouits@users.noreply.github.com>
2025-02-14 12:52:07 +0600

    part-4 custom logger with winston

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 
 import app from "./app.js";
-
+import logger from "./configs/logger.config.js";
 
 const PORT = process.env.PORT || 8000;
 
 
 app.listen(PORT, ()=> {
-    console.log(`server is listening at ${PORT}`);
+   //console.log(`server is listening at ${PORT}`);
+   //winstone makes error tracking,debugging,application monitoring
+   logger.info(`Server is listening at ${PORT}...`);
 });
