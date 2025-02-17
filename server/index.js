2f942e86160b6eed8ad02efb274b5a2725b7652b
Shuvo Bhowmik <41848224+Shuvouits@users.noreply.github.com>
2025-02-17 13:19:03 +0600

    part-7 connected mongodb database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,26 @@
-
+import mongoose from "mongoose";
 import app from "./app.js";
 import logger from "./configs/logger.config.js";
 
+//env variables
+const {DATABASE_URL} = process.env;
 const PORT = process.env.PORT || 8000;
+
+//exit on mongodb error
+
+mongoose.connection.on("error", (err)=> {
+   logger.error(`Mongodb Connection error : ${err}`);
+   process.exit(1);
+});
+
+//mongodb connection
+
+mongoose.connect(DATABASE_URL, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+}).then(()=> {
+   logger.info("Connected to Mongodb");
+})
 
 
 const server = app.listen(PORT, ()=> {
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -7,3 +7,7 @@ const router = express.Router();
 router.route("/register").post(trimRequest.all, register);
 
 export default router;
+
+//e4DoT6fmsP2rw9bR
+//mongodb+srv://shuvo:<db_password>@cluster0.5wbn0.mongodb.net/
+//mongodb+srv://shuvo:e4DoT6fmsP2rw9bR@cluster0.5wbn0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
