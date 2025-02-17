b9d954138add4adb754b64a6aa9d1fa879a03559
Shuvo Bhowmik <41848224+Shuvouits@users.noreply.github.com>
2025-02-17 14:02:30 +0600

    part-8 install react js and import svg icon demo

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import ChatIcon from "./svg/Chat";
+
+
+function App() {
+  return (
+    <div>
+       <h1>React js Project continue</h1>
+       <ChatIcon className="red" />
+    </div>
+  );
+}
+
+export default App;
