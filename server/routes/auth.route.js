3133b8c7af95a4e7b69855d4522cba1c64f10e75
Shuvo Bhowmik <41848224+Shuvouits@users.noreply.github.com>
2025-03-05 13:24:54 +0600

    part-19 creating new access token by using refresh token

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 import trimRequest from "trim-request"
-import { register, login, logout } from "../controllers/auth.controller.js";
+import { register, login, logout, refreshToken } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.route("/register").post(trimRequest.all, register);
 router.route("/login").post(trimRequest.all, login);
 router.route("/logout").post(trimRequest.all, logout);
+router.route("/refreshtoken").post(trimRequest.all, refreshToken);
 
 export default router;
 
