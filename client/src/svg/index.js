451d5888538f89b64562bb7be6a71aeca2d1877f
Shuvo Bhowmik <41848224+Shuvouits@users.noreply.github.com>
2025-03-21 15:49:24 +0600

    part-27 sidebar search component

--- client/src/svg/index.js
+++ client/src/svg/index.js
@@ -5,8 +5,14 @@ import StoryIcon from './Story';
 import NotificationIcon from './Notification';
 import ArrowIcon from './Arrow';
 import CloseIcon from './Close';
+import FilterIcon from './FilterIcon';
+import ReturnIcon from './Return';
+import SearchIcon from './Search'
 
 export{
+  SearchIcon,
+  ReturnIcon,
+  FilterIcon,
   ChatIcon,
   CommunityIcon,
   DotsIcon,
